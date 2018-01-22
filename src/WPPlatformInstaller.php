4bf1419c036457c69740f0be9f2e0ca41b1c33ca
LoreleiAurora <iam@l5i.me>
2018-01-22 17:10:04 +0000

    Symlink dropins to wp-content

--- src/WPPlatformInstaller.php
+++ src/WPPlatformInstaller.php
@@ -5,6 +5,7 @@ namespace HowToADHD\Composer;
 use Composer\Config;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
 
 class WPPlatformInstaller extends LibraryInstaller {
 
@@ -37,7 +38,7 @@ class WPPlatformInstaller extends LibraryInstaller {
 			$installationDir = $extra['wp-platform-install-dir'];
 		}
 		if ( ! $installationDir ) {
-			$installationDir = 'wp-content/platform';
+			$installationDir = $this->getContentPath() . '/platform';
 		}
 		$vendorDir = $this->composer->getConfig()->get( 'vendor-dir', Config::RELATIVE_PATHS ) ?: 'vendor';
 		if (
@@ -55,7 +56,96 @@ class WPPlatformInstaller extends LibraryInstaller {
 		}
 		self::$_installedPaths[ $installationDir ] = $prettyName;
 
-		return $installationDir;
+		return rtrim( $installationDir, '/\\' );
+	}
+
+	public function getContentPath() {
+		if ( $this->composer->getPackage() ) {
+			$topExtra = $this->composer->getPackage()->getExtra();
+			if ( ! empty( $topExtra['wp-content-dir'] ) ) {
+				return rtrim( $topExtra['wp-content-dir'], '/\\' );
+			}
+		}
+
+		return 'wp-content';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function install( InstalledRepositoryInterface $repo, PackageInterface $package ) {
+		parent::install( $repo, $package );
+		$this->linkDropins( $package );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function update( InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target ) {
+		$this->unlinkDropins( $initial );
+		parent::update( $repo, $initial, $target );
+		$this->linkDropins( $target );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function uninstall( InstalledRepositoryInterface $repo, PackageInterface $package ) {
+		$this->unlinkDropins( $package );
+		parent::uninstall( $repo, $package );
+	}
+
+	/**
+	 * Link dropins to wp-content.
+	 *
+	 * @param PackageInterface $package
+	 */
+	public function linkDropins( PackageInterface $package ) {
+		$content_dir = realpath( $this->getContentPath() );
+		$dropins_dir = realpath( $this->getInstallPath( $package ) . '/dropins/' );
+
+		// Link dropins not yet added.
+		foreach ( new \DirectoryIterator( $dropins_dir ) as $dropin ) {
+			if ( $dropin->isDot() ) {
+				continue;
+			}
+
+			$src  = $content_dir . '/' . $dropin->getFilename();
+			$dest = $dropins_dir . '/' . $dropin->getFilename();
+
+			if ( file_exists( $src ) ) {
+				$this->io->write( '<info>Unlinking ' . $this->filesystem->findShortestPath( $src, $dest ) . '</info>' );
+				unlink( $src );
+			}
+
+			$this->io->write( '<info>Linking ' . $this->filesystem->findShortestPath( $src, $dest ) . '</info>' );
+			$this->filesystem->relativeSymlink( $dest, $src );
+		}
+	}
+
+	/**
+	 * Unlink dropins from wp-content.
+	 *
+	 * @param PackageInterface $package
+	 */
+	public function unlinkDropins( PackageInterface $package ) {
+		$content_dir = realpath( $this->getContentPath() );
+		$dropins_dir = realpath( $this->getInstallPath( $package ) . '/dropins/' );
+
+		// Link dropins not yet added.
+		foreach ( new \DirectoryIterator( $dropins_dir ) as $dropin ) {
+			if ( $dropin->isDot() ) {
+				continue;
+			}
+
+			$src  = $content_dir . '/' . $dropin->getFilename();
+			$dest = $dropins_dir . '/' . $dropin->getFilename();
+
+			if ( file_exists( $src ) ) {
+				$this->io->write( '<info>Unlinking ' . $this->filesystem->findShortestPath( $src, $dest ) . '</info>' );
+				unlink( $src );
+			}
+		}
 	}
 
 	/**
--- src/WPPlatformPlugin.php
+++ src/WPPlatformPlugin.php
@@ -5,8 +5,16 @@ namespace HowToADHD\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Util\Filesystem;
 
 class WPPlatformPlugin implements PluginInterface {
+
+	/**
+	 * Composer Filesystem
+	 *
+	 * @var Filesystem
+	 */
+	private $filesystem;
 
 	/**
 	 * Apply plugin modifications to composer
@@ -15,10 +23,12 @@ class WPPlatformPlugin implements PluginInterface {
 	 * @param IOInterface $io
 	 */
 	public function activate( Composer $composer, IOInterface $io ) {
+		$this->io         = $io;
+		$this->filesystem = new Filesystem();
+
 		$installer = new WPPlatformInstaller( $io, $composer );
 		$composer->getInstallationManager()->addInstaller( $installer );
 		$installer = new WordPressCoreInstaller( $io, $composer );
 		$composer->getInstallationManager()->addInstaller( $installer );
 	}
-
 }
